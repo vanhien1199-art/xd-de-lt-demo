1f2aba964305e2d04e50cb6cf904769d1eeca429
vanhien1199-art <vanhien1199@gmail.com>
2026-02-27 11:06:20 +0700

    Update api_matrix.js

--- functions/api_matrix.js
+++ functions/api_matrix.js
@@ -603,7 +603,7 @@ if (finalSolution) {
       
       const genAI = new GoogleGenerativeAI(apiKey);
       const model = genAI.getGenerativeModel(
-        { model: 'gemini-2.5 pro' },
+        { model: 'gemini-2.5-Pro' },
         { baseUrl: 'https://gateway.ai.cloudflare.com/v1/a59c0991f0b291394bbe2fca8ba2694f/hien-demo/google-ai-studio' }
       );
 
@@ -677,4 +677,5 @@ if (finalSolution) {
     }
   }
 }
+
 
